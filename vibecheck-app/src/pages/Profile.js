3bcb979563c2937a602f08de668e9c8041bc4c6b
Sofia Dimos <sdimos12@gmail.com>
2020-11-04 17:37:02 -0500

    React app set up with styled-components and react-bulma-components

---
+++ vibecheck-app/src/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Profile = () => {
+    return (
+        <div>
+            <h1> PROFILE PAGE </h1>
+        </div>
+        
+    )
+}
+
+export default Profile
---
+++ vibecheck-app/src/pages/Statistics.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Statistics = () => {
+    return (
+        <div>
+            <h1> STATS PAGE </h1>
+        </div>
+        
+    )
+}
+
+export default Statistics
