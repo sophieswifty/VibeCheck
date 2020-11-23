73cba6a433cf4d0277de3bc26368166f141cb0ba
Dennis Schimtzek <dschimtz@ad.unc.edu>
2020-11-23 16:07:38 -0500

    statistics

--- vibecheck-app/src/pages/Statistics.js
+++ vibecheck-app/src/pages/Statistics.js
@@ -1,14 +1,15 @@
 import React, {useContext, useState} from 'react';
 import UserDataContext from '../context/userdata';
 import StatisticsBox from '../components/StatisticsBox';
-import { getUserTopTracks } from '../API/spotifyAPI';
+// import StatisticsPanel from '../components/StatisticsPanel';
 import './Statistics.css'
 
 function Statistics() {
     const userData = useContext(UserDataContext);
     return (
         <div>
-         <StatisticsBox userData={userData}></StatisticsBox>
+            {/* <StatisticsPanel userData={userData}></StatisticsPanel> */}
+            <StatisticsBox userData={userData}></StatisticsBox>
         </div>
     )
 }
