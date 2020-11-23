25d6f9b7e19f942191c5aeb95c38f9d658009816
Ujesh Regmi <ujeshregmi@gmail.com>
2020-11-23 18:23:30 -0500

    backend Functions file

---
+++ vibecheck-app/src/backendFns.js
@@ -0,0 +1,45 @@
+import axios from "axios"
+
+let userInfo; // The user info object
+let playlistInfo; // The spotify playlist object that is generated when they create playlist
+let addPlaylistToBackend = async (userInfo, playlistInfo) => {
+    await axios({
+        method: 'POST', 
+        url: 'https://vibecheck-please.herokuapp.com/playlists',
+        body: {
+            "playlistID": "", // Spotify Playlist ID -> playlistInfo.id
+            "playlistURL": "", // Spotify Playlist URL -> playlistInfo.external_urls.spotify
+            "playlistName": "", // Spotify Playlist Name -> playlistInfo.name
+            "playlistIMG": "", // Spotify Playlist IMG -> playlistInfo.images[0].url
+            "userID": "", // Spotify Username -> userInfo.id
+            "displayName": "" // Spotify Profile Display Name -> userInfo.display_name
+        }
+
+    })
+}
+
+//Takes in user ID
+//Deletes all playlists created by that user
+let deleteAllUserPlaylists = async (userID) => {
+    //Iterate through entire backend
+    axios.get('https://vibecheck-please.herokuapp.com/playlists/all').then( (res) => {
+        for (let i = 0; i < res.length; i++) {
+            if (res[i].userID == userID) {
+                axios.delete(`https://vibecheck-please.herokuapp.com/playlists/${res[i].backendID}`);
+            }
+        }
+    });
+}
+
+//Takes in the user ID and the playlist ID
+//Deletes specific playlist made by user
+let deleteSpecificPlaylist = async (userID, playlistID) => {
+    axios.get('https://vibecheck-please.herokuapp.com/playlists/all').then( (res) => {
+        for (let i = 0; i < res.length; i++) {
+            if (res[i].userID == userID && res[i].playlistID == playlistID) {
+                axios.delete(`https://vibecheck-please.herokuapp.com/playlists/${res[i].backendID}`);
+                break;
+            }
+        }
+    });
+}
